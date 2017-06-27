7587fa0ad5fd28c82656c93053f6c764131d2168
surov <brainsurov@gmail.com>
2017-06-27 13:34:17 +0300

    Add loadMore to feedContainer

--- src/scripts/containers/Feed.js
+++ src/scripts/containers/Feed.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Button} from 'react-toolbox/lib/button';
 
 import Stream from './Stream';
 
@@ -12,19 +13,31 @@ const mapStateToProps = store => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchAndReceiveGlobalStreams: filters => () => dispatch(fetchAndReceiveGlobalStreams(filters))
+  fetchAndReceiveGlobalStreams: filters => dispatch(fetchAndReceiveGlobalStreams(filters))
 });
 
 class Feed extends Component {
 
+  state = {
+    filters: {
+      limit: 5,
+      offset: 0
+    },
+    fetchedAll: false
+  }
+
+  incrementOffsetFilter = (n = 5) => this.setState({filters:{...this.state.filters, offset: this.state.filters.offset + n}});
+  loadMoreStreams = () => this.props.fetchAndReceiveGlobalStreams(this.state.filters).then(_ => this.incrementOffsetFilter(5));
+
   componentDidMount(){
-    return this.props.fetchAndReceiveGlobalStreams(this.props.global.filters)();
+    return this.loadMoreStreams();
   }
 
   render(){
     return (
       <div className={styles.streams}>
-        {this.props.global.items.map(s => <Stream id={s} key={s} />)}
+        {this.props.global.map(s => <Stream id={s} key={s} />)}
+        <Button label='load more' raised primary onClick={this.loadMoreStreams} />
       </div>
     );
   }
