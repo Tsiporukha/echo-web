9a81f293a165eb3a275a48ce9188b3182827aeae
surov <brainsurov@gmail.com>
2017-07-13 15:17:44 +0300

    Add IndeterminateProgressLine component
    and effects.css

---
+++ src/scripts/components/IndeterminateProgressLine.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+import effects from '../../assets/styles/effects.css';
+
+export const doWithProgressLine = (action, setVisibility) => Promise.resolve(setVisibility(true)).then(action).then(_ => setVisibility(false));
+
+const IndeterminateProgressLine = ({visible, className = ''}) => (visible ? <div className={className ? className : effects.progressLine} /> : false);
+
+export default IndeterminateProgressLine;
