a17124122ceb7c059159022578b2bb5018739ca6
surov <brainsurov@gmail.com>
2017-07-13 16:00:22 +0300

    Update FeedSources component
    Now available for feed and search modes

--- src/scripts/components/FeedSources.js
+++ src/scripts/components/FeedSources.js
@@ -1,35 +1,55 @@
 import React, {Component} from 'react';
-import {Tab, Tabs} from 'react-toolbox';
+import {Tab, Tabs} from 'react-toolbox/lib/tabs';
 
 import Feed from '../containers/Feed';
+import FeedSearch from '../containers/FeedSearch';
+import Youtube from '../containers/Youtube';
+import Soundcloud from '../containers/Soundcloud';
 
 import styles from '../../assets/styles/feed.css';
 import tabsTheme from '../../assets/styles/tabsTheme.css';
 
+const getFeedSourcesTabs = (search, activeTabIndex, handleTabChange) => (search ?
+  <Tabs theme={tabsTheme} index={activeTabIndex} onChange={handleTabChange}>
+    <Tab label={<i className={styles.youtubeIcon} />}> <Youtube /> </Tab>
+    <Tab label={<i className={styles.soundcloudIcon} />}> <Soundcloud /> </Tab>
+    <Tab label={<i className={styles.feedIcon}>language</i>}> <FeedSearch /> </Tab>
+    <Tab label={<i className={styles.whatshotIcon}>whatshot</i>}><small>whatshot</small></Tab>
+  </Tabs>
+  :
+  <Tabs theme={tabsTheme} index={activeTabIndex} onChange={handleTabChange}>
+    <Tab label={<i className={styles.feedIcon}>language</i>}> <Feed /> </Tab>
+    <Tab label={<i className={styles.whatshotIcon}>whatshot</i>}><small>whatshot</small></Tab>
+  </Tabs>
+);
+
+const maybeSearchResultsPlaceholder = search => search ?
+  (<div className={styles.searchResultsPlaceholder}>
+    <i className={styles.searchIcon}>search</i> <br />
+    <span>Your search results appear here.</span>
+  </div>)
+  : false;
+
 export default class FeedSources extends Component {
 
-  state = {index: 0}
-
   handleTabChange = index => this.setState({index});
+
+
+  state = {index: 0}
 
   render(){
     return (
       <section>
         <div className={styles.root}>
           <div className={styles.leftReg}>
+            {maybeSearchResultsPlaceholder(this.props.search)}
+
             <div className={styles.feedSourceContent}>
-              <Tabs theme={tabsTheme} index={this.state.index} onChange={this.handleTabChange}>
-                <Tab label={<i className={styles.feedIcon}>language</i>}>
-                  <Feed />
-                </Tab>
-                <Tab label={<i className={styles.whatshotIcon}>whatshot</i>}><small>whatshot</small></Tab>
-              </Tabs>
+              {getFeedSourcesTabs(this.props.search, this.state.index, this.handleTabChange)}
             </div>
           </div>
+
         </div>
-
-
-
       </section>
     );
   }
