ddab1fc9d99632eb336f624c3eb82be27ce79ac5
surov <brainsurov@gmail.com>
2017-07-19 17:47:24 +0300

    Add Login and MaybeCurrentUser containers

---
+++ src/scripts/containers/Login.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+import {emailLogin, networkLogin} from '../actions/SessionActions';
+
+import styles from '../../assets/styles/login.css';
+
+
+const mapDispatchToProps = dispatch => ({
+  emailLogin: (email, password) => dispatch(emailLogin(email, password)),
+  networkLogin: (token, network) => dispatch(networkLogin(token, network))
+});
+
+
+class Login extends Component {
+
+  static propTypes = {
+    emailLogin: PropTypes.func,
+    networkLogin: PropTypes.func
+  };
+
+
+  onFacebookLoginStatus = response =>
+    response.status === 'connected' ?
+      this.props.networkLogin(response.authResponse.accessToken, 'facebook') :
+      window.FB.login(response => response.status === 'connected' ? this.onFacebookLoginStatus(response) : false);
+
+  facebookLogin = () => window.FB.getLoginStatus(this.onFacebookLoginStatus);
+
+  emailLogin = () => this.props.emailLogin(this.refs.email.value, this.refs.password.value)
+    .catch(_e => this.setState({invalidCredentials: true}));
+  onSubmit = e => Promise.resolve(e.preventDefault()).then(this.emailLogin);
+
+
+  state = {invalidCredentials: false};
+
+  render(){
+    return (
+      <div className={styles.root}>
+        <div className={styles.signIn}>Sign in with:</div>
+
+        <button className={styles.btnFb} onClick={this.facebookLogin}>Facebook</button>
+        <div className={styles.orDivider}>or</div>
+        <form onSubmit={this.onSubmit}>
+          <div className={styles.input}>
+            <input ref='email' required type='email' placeholder='email' />
+          </div>
+          <div className={styles.input}>
+            <input ref='password' required type='password' placeholder='password' />
+          </div>
+
+          <div className={styles.ebArea}>
+            {this.state.invalidCredentials && <p className={styles.err}>Incorrect email or password.</p>}
+            <button type='submit' className={styles.btn}>Login</button>
+          </div>
+        </form>
+
+        <div className={styles.mobileArea}>
+          <div>
+            <b>Need an account?</b><br />
+            Download the app to sign up:
+          </div>
+
+          <div className={styles.btns}>
+            <a href='https://itunes.apple.com/us/app/echo-sync-your-devices-in/id772203328?mt=8' target='_blank'>
+              <button className={styles.btn}>
+                <span className={styles.firstLine}>Download on the</span><br />
+                <span className={styles.secondLine}>App Store</span>
+              </button>
+            </a>
+            <a href='http://play.google.com/store/apps/details?id=com.echo.app' target='_blank'>
+              <button className={styles.btn}>
+                <span className={styles.firstLine}>Get it on</span><br />
+                <span className={styles.secondLine}>Google Play</span>
+              </button>
+            </a>
+
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(() => ({}), mapDispatchToProps)(Login);
---
+++ src/scripts/containers/MaybeCurrentUser.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {Button} from 'react-toolbox/lib/button';
+import Dialog from 'react-toolbox/lib/dialog';
+
+import Login from './Login';
+
+import {clearSession} from '../actions/SessionActions';
+
+import styles from '../../assets/styles/maybeCurrentUser.css';
+
+
+const mapStateToProps = store => ({
+  token: store.session.token,
+  user: store.session.user
+});
+
+const mapDispatchToProps = dispatch => ({
+  clearSession: () => dispatch(clearSession())
+});
+
+class MaybeCurrentUser extends Component {
+
+  static propTypes = {
+    token: PropTypes.string,
+    user:  PropTypes.object,
+
+    clearSession: PropTypes.func
+  }
+
+  toggleDialogVisibility = () => this.setState({dialogVisibility: !this.state.dialogVisibility});
+
+
+  state = {dialogVisibility: false};
+
+  render(){
+    return (
+      <span className={styles.root}>
+        {this.props.token ?
+          <span className={styles.userArea}>
+            <Link to={`/profile/${this.props.user.id}`}>
+              <img src={this.props.user.avatar_url} alt='avatar' className={styles.avatar} />
+            </Link>
+          </span>
+          :
+          <span>
+            <Button icon='account_circle' raised
+              theme={styles}
+              onClick={this.toggleDialogVisibility}> <span className={styles.signInTitle}>Sign in</span> </Button>
+
+            <Dialog
+              active={this.state.dialogVisibility}
+              onEscKeyDown={this.toggleDialogVisibility}
+            >
+              <Login />
+            </Dialog>
+          </span>
+        }
+      </span>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MaybeCurrentUser);
