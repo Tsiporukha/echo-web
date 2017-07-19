8f541b980aca609da1d86e5587f4e9941635edb6
surov <brainsurov@gmail.com>
2017-07-19 17:39:51 +0300

    Add component for FacebookSDK loading

---
+++ src/scripts/components/FacebookSDK.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+import {FACEBOOK_APP_ID} from '../constants/skeys.js';
+
+export default class FacebookSDK extends Component {
+
+  loadSdk = () => (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_UK/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  asyncInit = () => {
+    window.fbAsyncInit = () => FB.init({
+      appId: FACEBOOK_APP_ID,
+      status: true,
+      cookie: true,
+      xfbml: true,
+      version: 'v2.10'
+    });
+  };
+
+  componentWillMount = () => Promise.resolve(this.loadSdk()).then(this.asyncInit());
+
+  render(){ return null; }
+}
