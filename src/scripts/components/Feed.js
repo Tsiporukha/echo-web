b3059c89ec1bcda0108543c8334dc874b119d01b
surov <brainsurov@gmail.com>
2017-08-01 12:08:07 +0300

    Add tags to Feed component

--- src/scripts/components/Feed.js
+++ src/scripts/components/Feed.js
@@ -10,6 +10,8 @@ import {onBottomReaching} from '../lib/scroll';
 import styles from '../../assets/styles/feed.css';
 import {results as resultsClassName} from '../../assets/styles/search.css';
 
+import tags from '../../assets/tags.json';
+const primaryTags = Object.keys(tags);
 
 export default class Feed extends Component {
 
@@ -40,12 +42,18 @@ export default class Feed extends Component {
     ((nextProps.initialFilters.term !== props.initialFilters.term) || (nextProps.token !== props.token)) ?
       this.onSearchTermChange(nextProps.initialFilters, nextProps.token) : false;
 
+  addTag = tag => this.setState({tags: this.state.tags.concat(tag)});
+  removeTag = tag => this.setState({tags: this.state.tags.filter(tg => tg !== tag)});
+  isInTags = tag => this.state.tags.includes(tag);
+  getTagOnClick = tag => () => (this.isInTags(tag) ? this.removeTag : this.addTag)(tag);
+
 
   state = {
     filters: this.props.initialFilters,
     fetchedAll: false,
     activeSubFeed: 0,
-    searching: false
+    searching: false,
+    tags: [],
   }
 
   componentWillMount = () => this.loadSubFeedStreams(this.state.activeSubFeed)(this.props.token);
@@ -65,6 +73,11 @@ export default class Feed extends Component {
             </div>
           </Tab>
           <Tab label='Latest' onActive={this.loadMoreLatestStreams}>
+            {this.props.initialFilters.term === undefined && <div className={styles.tags}>
+              {primaryTags.map(ptag =>
+                <span key={ptag} className={this.isInTags(ptag) ? styles.selectedTag : styles.tag} onClick={this.getTagOnClick(ptag)}>{ptag}</span>
+              )}
+            </div>}
             <div className={styles.streams}>
               {this.props.latest.map(s => <Stream id={s} key={s} />)}
             </div>
