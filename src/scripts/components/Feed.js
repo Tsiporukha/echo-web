001337179ae9519f883efadd44912bb6283bb73b
surov <brainsurov@gmail.com>
2017-08-07 13:37:40 +0300

    Add tags filter to latest sub feed in Feed component

--- src/scripts/components/Feed.js
+++ src/scripts/components/Feed.js
@@ -15,12 +15,13 @@ const primaryTags = Object.keys(tags);
 
 export default class Feed extends Component {
 
+  reinitializeFilters = () => this.setState({filters: this.props.initialFilters});
   handleTabChange = activeSubFeed => this.setState({filters: this.props.initialFilters, activeSubFeed});
 
   incrementOffsetFilter = (n = this.state.filters.limit) => this.setState({filters:{...this.state.filters, offset: this.state.filters.offset + n}});
   setSearchingVisibility = searching => this.setState({searching});
   loadMoreStreams = action => (token = this.props.token) =>
-    doWithProgressLine(() => this.props.fetchAndReceiveStreams(action)(this.state.filters, token), this.setSearchingVisibility)
+    doWithProgressLine(() => this.props.fetchAndReceiveStreams(action)({...this.state.filters, tags: this.state.tags}, token), this.setSearchingVisibility)
       .then(this.incrementOffsetFilter);
 
   loadMoreLatestStreams = this.loadMoreStreams(this.props.fetchAndReceiveLatestStreamsAction);
@@ -28,12 +29,13 @@ export default class Feed extends Component {
   loadMoreLongestStreams = this.loadMoreStreams(this.props.fetchAndReceiveLongestStreamsAction);
 
   loadSubFeedStreams = subFeedIndex => token => [this.loadMorePopularStreams, this.loadMoreLatestStreams, this.loadMoreLongestStreams][subFeedIndex](token);
+  loadActiveSubFeedStreams = () => this.loadSubFeedStreams(this.state.activeSubFeed)(this.props.token);
 
   onSearchTermChange = (filters, token) => Promise.resolve(this.setState({filters})).then(_ => this.loadSubFeedStreams(this.state.activeSubFeed)(token));
 
   dispatchScrollListener = (() => {
     const element = document.getElementsByClassName(resultsClassName)[0] || window;
-    const handleScroll = () => onBottomReaching(() => this.loadSubFeedStreams(this.state.activeSubFeed)(this.props.token), element);
+    const handleScroll = () => onBottomReaching(() => this.loadActiveSubFeedStreams(), element);
 
     return actionName => element[actionName]('scroll', handleScroll);
   })();
@@ -44,8 +46,9 @@ export default class Feed extends Component {
 
   addTag = tag => this.setState({tags: this.state.tags.concat(tag)});
   removeTag = tag => this.setState({tags: this.state.tags.filter(tg => tg !== tag)});
-  isInTags = tag => this.state.tags.includes(tag);
-  getTagOnClick = tag => () => (this.isInTags(tag) ? this.removeTag : this.addTag)(tag);
+  inTags = tag => this.state.tags.includes(tag);
+  handleTagClick = tag => () => Promise.resolve((this.inTags(tag) ? this.removeTag : this.addTag)(tag))
+    .then(this.reinitializeFilters).then(this.loadActiveSubFeedStreams);
 
 
   state = {
@@ -56,7 +59,7 @@ export default class Feed extends Component {
     tags: [],
   }
 
-  componentWillMount = () => this.loadSubFeedStreams(this.state.activeSubFeed)(this.props.token);
+  componentWillMount = () => this.loadActiveSubFeedStreams();
 
   componentDidMount = () => this.dispatchScrollListener('addEventListener');
   componentWillUnmount = () => this.dispatchScrollListener('removeEventListener');
@@ -75,7 +78,7 @@ export default class Feed extends Component {
           <Tab label='Latest' onActive={this.loadMoreLatestStreams}>
             {this.props.initialFilters.term === undefined && <div className={styles.tags}>
               {primaryTags.map(ptag =>
-                <span key={ptag} className={this.isInTags(ptag) ? styles.selectedTag : styles.tag} onClick={this.getTagOnClick(ptag)}>{ptag}</span>
+                <span key={ptag} className={this.inTags(ptag) ? styles.selectedTag : styles.tag} onClick={this.handleTagClick(ptag)}>{ptag}</span>
               )}
             </div>}
             <div className={styles.streams}>
