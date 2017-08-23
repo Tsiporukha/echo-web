d4b0f68577d66df05fedd02a2ff176a4275c8d70
surov <brainsurov@gmail.com>
2017-08-23 14:14:03 +0300

    Add PopularSongs container

---
+++ src/scripts/containers/PopularSongs.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import Song from './Song';
+
+import {addSongs} from '../actions/EntitiesAUDActions';
+
+import {reduceToObject} from '../lib/stream';
+import {getPopular} from '../lib/ebApi/songs';
+
+
+const mapStateToProps = state => ({
+  token: state.session.token,
+  term: state.search.term,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchAndReceiveSongs: (term, limit, offset, token) => getPopular(term, limit, offset, token)
+    .then(({songs}) => Promise.resolve(dispatch(addSongs(reduceToObject(songs)))).then(_ => songs.map(song => song.id))),
+});
+
+class PopularSongs extends Component {
+
+  fetchAndReceiveSongsIds = (term = this.props.term, token = this.props.token) =>
+    this.props.fetchAndReceiveSongs(term, this.state.limit, this.state.offset, token).then(songs => this.setState({songs}));
+
+  maybeReloadOnPropsChange = (nextProps, props = this.props) =>
+    ((nextProps.term !== props.term) || (nextProps.token !== props.token)) ?
+      this.fetchAndReceiveSongsIds(nextProps.term, nextProps.token) : false;
+
+
+  componentWillMount = () => this.fetchAndReceiveSongsIds();
+
+  componentWillReceiveProps = nextProps => this.maybeReloadOnPropsChange(nextProps, this.props)
+
+  state = {
+    songs: [],
+
+    limit: 14,
+    offset: 0,
+  };
+
+  render() {
+    return(
+      <div>
+        {this.state.songs.map(songId => <Song id={songId} key={songId} />)}
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PopularSongs);
--- src/scripts/lib/ebApi/songs.js
+++ src/scripts/lib/ebApi/songs.js
@@ -1,4 +1,9 @@
 import luch, {getJson} from 'luch';
-import {getAbsoluteUrl} from './api';
+import {getAbsoluteUrl, withoutUndefinedParams} from './api';
 
 export const toggleLike = (id, token) => luch.post(getAbsoluteUrl(`/songs/${id}/toggle_like`), {token});
+
+export const getPopular = (term, limit, offset, token) =>
+  luch(getAbsoluteUrl(`/songs/popular?limit=${limit}&offset=${offset}` +
+    `${token ? `&token=${token}` : ''}${term ? `&term=${term}` : ''}`))
+      .then(getJson);
