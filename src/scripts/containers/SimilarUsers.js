658c51199d9f294454439ad21270b6d0c62a99e6
surov <brainsurov@gmail.com>
2017-09-13 14:28:30 +0300

    Add SimilarUsers container

---
+++ src/scripts/containers/SimilarUsers.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import User from './User';
+
+import {Button} from 'react-toolbox';
+
+import {addUsers, followUser, unfollowUser} from '../actions/EntitiesAUDActions';
+
+import {reduceToObject} from '../lib/stream';
+import {getSimilar} from '../lib/ebApi/users';
+
+import styles from '../../assets/styles/profile.css';
+
+
+const mapStateToProps = (state, ownProps) => ({
+  token: state.session.token,
+});
+
+const mapDispatchToProps = dispatch => ({
+  addUsers: users => dispatch(addUsers(reduceToObject(users))),
+  follow: (user, token) => dispatch(followUser(user, token)),
+  unfollow: (user, token) => dispatch(unfollowUser(user, token)),
+});
+
+
+const initialState = {
+  users: [],
+};
+
+class SimilarUsers extends Component {
+
+  initialLoad = (userId = this.props.userId, token = this.props.token) => getSimilar(userId, token)
+    .then(({results}) => Promise.resolve(this.props.addUsers(results)).then(_ => this.setState({users: results.map(usr => usr.id)})));
+
+  reinitOnUserChange = (nextProps, props) => nextProps.userId !== props.userId ?
+    Promise.resolve(this.setState(initialState)).then(_ => this.initialLoad(nextProps.userId, nextProps.token)) : false;
+
+
+  state = {...initialState};
+
+  componentWillMount = () => this.initialLoad(this.props.userId, this.props.token);
+
+  componentWillReceiveProps = nextProps => this.reinitOnUserChange(nextProps, this.props);
+
+  render() {
+    return(
+      !!this.state.users.length &&
+      <div className={styles.similarUsers}>
+        <div className={styles.head}>SIMILAR USERS</div>
+        {this.state.users.map(userId => <User key={userId} id={userId} />)}
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SimilarUsers);
---
+++ src/scripts/containers/User.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+import {Button, Tab, Tabs} from 'react-toolbox';
+
+import Stream from './Stream';
+import UserLikes from './UserLikes';
+import SimilarUsers from './SimilarUsers';
+
+import {followUser, unfollowUser} from '../actions/EntitiesAUDActions';
+
+import styles from '../../assets/styles/profile.css';
+
+
+const mapStateToProps = (state, ownProps) => ({
+  user: state.users[ownProps.id],
+  token: state.session.token,
+  currentUser: state.session.user,
+});
+
+const mapDispatchToProps = dispatch => ({
+  follow: (user, token) => () => dispatch(followUser(user, token)),
+  unfollow: (user, token) => () => dispatch(unfollowUser(user, token)),
+});
+
+
+const User = props => (
+  <div className={styles.userInfo}>
+    <Link to={`/profile/${props.user.id}`}>
+      <img src={props.user.avatar_url} alt='avatar' className={styles.avatar} />
+    </Link>
+    <div className={styles.info}>
+      <Link to={`/profile/${props.user.id}`}>
+        <div className={styles.name}>{props.user.name}</div>
+      </Link>
+      <div className={styles.followersCount}>{props.user.followers_count} followers</div>
+    </div>
+    {props.token && (props.user.id !== props.currentUser.id) && (props.user.is_followed ?
+      <Button onClick={props.unfollow(props.user, props.token)} className={styles.followedBtn} icon='done' label='followed' raised primary /> :
+      <Button onClick={props.follow(props.user, props.token)} className={styles.followBtn} icon='add' label='follow' raised primary />
+    )}
+  </div>
+)
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(User);
