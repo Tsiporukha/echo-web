de9cbe8f2804d9501bf938346c1e8a4dd92da7ea
surov <brainsurov@gmail.com>
2017-09-20 16:57:47 +0300

    Add WelcomeNote and RecentlyLiked

---
+++ src/scripts/components/WelcomeNote.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+
+import styles from '../../assets/styles/feed.css';
+
+const WelcomeNote = _ => (
+  <div className={styles.welcomeNote}>
+    <h2>Welcome to Echo (FAQ):</h2>
+
+    <div>
+      <h4>1. What is Echo? </h4>
+      It’s a daily-updated source for music
+      crowdsourced by our music loving
+      community of users.
+    </div>
+
+    <div>
+      <h4>2. What are Music Rooms?</h4>
+      Each room is a themed playlist managed and  updated by users on a daily basis. The theme can be anything: a genre, a mood, a decade
+      (Synthwave, Dark, 80s, HipHop, TripHop, Ethnic).
+      Rooms can be public, shared or private.
+    </div>
+
+    <div>
+      <h4>3. # What are Tags?</h4>
+      When creating a Room users assign tags to playlists for in order to find them easily.
+      Simply click on a tag in the feed and it will show all Rooms containing the tag
+      (for ex:Electronic).
+    </div>
+
+    <div>
+      <h4>4. ➕ How do I add tracks to Rooms?</h4>
+      Show me>>On Echo you can search through YouTube, SoundCloud, Spotify and Deezer to find tracks, play and add them to Rooms.
+    </div>
+
+    <div>
+      <h4>5. How do I listen to the freaking Music?!?</h4>
+      If you click on “Play” or “Add to Queue”
+      anywhere it will appear in the Queue.
+      You can add a bunch of songs to the Queue, enjoy them and share (if you want) with the rest of Echo by saving them in a Public Room.
+
+    </div>
+
+  </div>
+);
+
+export default WelcomeNote;
---
+++ src/scripts/containers/RecentlyLiked.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import {Tab, Tabs} from 'react-toolbox';
+
+import RecentlyLikedSong from './RecentlyLikedSong';
+import RecentlyLikedStream from './RecentlyLikedStream';
+import WelcomeNote from '../components/WelcomeNote';
+
+import {fetchAndReceiveLikedStreamsIds, fetchAndReceiveLikedSongsIds} from '../actions/EntitiesAUDActions';
+
+import {getLikedSongs, getLikedStreams} from '../lib/ebApi/users';
+
+import styles from '../../assets/styles/feed.css';
+
+
+const RecentlyLikedItems = ({songs, streams}) => (
+  <div className={styles.recentlyLiked}>
+    {!!streams.length && <div className={styles.streams}>
+      <div className={styles.itemsTitle}>RECENTLY LIKED PLAYLISTS</div>
+      {streams.map(streamId => <RecentlyLikedStream id={streamId} key={streamId} />)}
+    </div>}
+    {!!songs.length && <div className={styles.songs}>
+      <div className={styles.itemsTitle}>RECENTLY LIKED SONGS</div>
+      {songs.map(songId => <RecentlyLikedSong id={songId} key={songId} />)}
+    </div>}
+  </div>
+);
+
+
+const mapStateToProps = state => ({
+  token: state.session.token,
+  userId: state.session.user && state.session.user.id,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchAndReceiveStreamsIds: (userId, limit, offset, token) => dispatch(fetchAndReceiveLikedStreamsIds(userId, limit, offset, token)),
+  fetchAndReceiveSongsIds: (userId, limit, offset, token) => dispatch(fetchAndReceiveLikedSongsIds(userId, limit, offset, token)),
+});
+
+
+const emptyItems = {
+  songs: [],
+  streams: [],
+};
+
+class RecentlyLiked extends Component {
+
+  clearLikes = () => this.setState(emptyItems);
+
+  fetchAndReceiveStreamsIds = (userId = this.props.userId, token = this.props.token) =>
+    this.props.fetchAndReceiveStreamsIds(userId, this.state.limit, this.state.offset, token).then(streams => this.setState({streams}));
+
+  fetchAndReceiveSongsIds = (userId = this.props.userId, token = this.props.token) =>
+    this.props.fetchAndReceiveSongsIds(userId, this.state.limit, this.state.offset, token).then(songs => this.setState({songs}));
+
+  fetchAndReceiveItems = (userId = this.props.userId, token = this.props.token) =>
+    Promise.all([this.fetchAndReceiveStreamsIds(userId, token), this.fetchAndReceiveSongsIds(userId, token)]);
+
+
+  state = {
+    ...emptyItems,
+
+    limit: 4,
+    offset: 0,
+  };
+
+  componentWillMount = () => this.props.token && this.fetchAndReceiveItems();
+
+  componentWillReceiveProps = nextProps => nextProps.token ? this.fetchAndReceiveItems(nextProps.userId, nextProps.token) : this.clearLikes();
+
+  render() {
+    return(
+      this.state.songs.length || this.state.streams.length ?
+        <RecentlyLikedItems songs={this.state.songs} streams={this.state.streams} /> :
+        <WelcomeNote />
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecentlyLiked);
