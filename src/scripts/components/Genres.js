80874505516d9bf4f19613e1f31ed63ed67c4884
surov <brainsurov@gmail.com>
2017-11-16 16:01:34 +0200

    Update Genres component

--- src/scripts/components/Genres.js
+++ src/scripts/components/Genres.js
@@ -1,20 +1,48 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {Button} from 'react-toolbox/lib/button';
 
-import {genresNames} from '../lib/genres';
+import ShareIconMenu from './ShareIconMenu';
 
+import {genres} from '../lib/genres';
 
-const GenreCard = props => (
-  <Link to={`/genres/${props.title}`}>
-    <div>
-      {props.title}
+import styles from '../../assets/styles/genres.css';
+import cardStyles from '../../assets/styles/card.css';
+
+export const GenreCard = ({genre}) => (
+  <div className={cardStyles.root}>
+    <img className={cardStyles.artwork} src={genre.artwork_url} alt='genre artwork' />
+    <div className={cardStyles.data}>
+      <div className={cardStyles.titleBlock}>
+        <div className={cardStyles.title}>
+          <Link to={`/genres/${genre.title}`}> {genre.title} </Link>
+        </div>
+        <ShareIconMenu path={`/genres/${genre.title}`} picture={genre.artwork_url}
+          title={genre.title} description={genre.description} />
+      </div>
+      <div className={cardStyles.description}> {genre.description} </div>
     </div>
-  </Link>
+  </div>
 )
+
+const MiniWelcomeNote = () => (
+  <div className={styles.miniWelcomeNote}>
+    <div className={styles.welcome}>Welcome to Echo!</div>
+    <div className={styles.noteBody}>
+      <div className={styles.note}>
+        We are a collection of music communities. Browse through the genres
+        and find fresh, events, venues, culture etc. Sign up to create your
+        own library and get updates from communities that interest you.
+      </div>
+      <Button raised> <span className={styles.signInTitle}>Sign in</span> </Button>
+    </div>
+  </div>
+);
 
 const Genres = props => (
   <div>
-    {genresNames.map(genreName => <GenreCard key={genreName} title={genreName} />)}
+    <MiniWelcomeNote />
+    {genres.map(genre => <GenreCard key={genre.title} genre={genre} />)}
   </div>
 )
 
