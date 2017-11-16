91980ca56fd52b6f108234dfb5841a2cd9070f3b
surov <brainsurov@gmail.com>
2017-11-16 16:00:50 +0200

    Update Genre component

--- src/scripts/components/Genre.js
+++ src/scripts/components/Genre.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 import ShareIconMenu from './ShareIconMenu';
+import {GenreCard} from './Genres';
 
-import {getGenre, getSecondaryTags} from '../lib/genres';
+import {getGenre, getSecondaryTags, genres} from '../lib/genres';
 
 import styles from '../../assets/styles/genre.css';
+import cardStyles from '../../assets/styles/card.css';
 
 
 const RoomPreview = props => <div>
@@ -27,19 +29,21 @@ class Genre extends Component {
       <div className={styles.genre}>
 
         <div className={styles.genreHeader} style={{backgroundImage: `url(${this.genre.background_url})`}}>
-          <div className={styles.genreData}>
-            <img className={styles.artwork} src={this.genre.artwork_url} alt='' />
-            <div className={styles.text}>
-              <div className={styles.title}>
-                <span className={styles.title}>{this.genre.title} Music</span>
-                <span className={styles.request}>REQUEST TO ADD ROOM</span>
-              </div>
-              <div className={styles.description}>
-                <i>{this.genre.description}</i>
-              </div>
-              <div className={styles.iconArea}>
-                <ShareIconMenu path={`/genres/${this.genre.title}`} picture={this.genre.artwork_url}
-                  title={this.genre.title} description={this.genre.description} />
+          <div className={styles.genreContent}>
+            <div className={styles.genreCard}>
+              <img className={cardStyles.artwork} src={this.genre.artwork_url} alt='' />
+              <div className={cardStyles.data}>
+                <div className={cardStyles.titleBlock}>
+                  <span className={cardStyles.title}>{this.genre.title} Music</span>
+                  <span className={styles.requestSpan}>REQUEST TO ADD ROOM</span>
+                </div>
+                <div className={cardStyles.description}>
+                  <i>{this.genre.description}</i>
+                </div>
+                <div className={cardStyles.iconArea}>
+                  <ShareIconMenu path={`/genres/${this.genre.title}`} picture={this.genre.artwork_url}
+                    title={this.genre.title} description={this.genre.description} />
+                </div>
               </div>
             </div>
           </div>
@@ -51,13 +55,14 @@ class Genre extends Component {
 
 
         <div className={styles.left}>
+          <div className={styles.fullHeight} />
           <div className={styles.leftContent}>
             <div className={styles.tags}>
               <span className={styles.t}>Tags:</span>
               {this.tags.map(tag => (<span key={tag} className={styles.tag}>#{tag}</span>))}
             </div>
 
-            {this.state.rooms.map(room => <RoomPreview room={room} />)}
+            {genres.map(genre => <GenreCard key={genre.title} genre={genre} />)}
           </div>
         </div>
 
