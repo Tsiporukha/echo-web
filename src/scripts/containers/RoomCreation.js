2bbe176a547a31485cead83948bf37f58ca77907
surov <brainsurov@gmail.com>
2017-11-30 11:25:28 +0200

    Add RoomCreation container

---
+++ src/scripts/containers/RoomCreation.js
@@ -0,0 +1,20 @@
+import {connect} from 'react-redux';
+
+import RoomEditing from '../components/RoomEditing';
+
+import {getQueueSongs} from '../actions/PlayerActions';
+import {create} from '../lib/ebApi/rooms';
+
+
+const mapStateToProps = (state, ownProps) => ({
+  songs: getQueueSongs(state),
+  token: state.session.token
+});
+
+const mapDispatchToProps = dispatch => ({
+  save: (artwork_url, background_url, title, description, genre, tags, songs, token) =>
+    create(artwork_url, background_url, title, description, genre, tags, songs, token),
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(RoomEditing);
