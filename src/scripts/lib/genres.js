28fc84f152bb8724f0638124d95c8496ff409ba5
surov <brainsurov@gmail.com>
2017-12-05 14:30:20 +0200

    Replace old tags in tags.json with newTags.json

--- src/scripts/lib/genres.js
+++ src/scripts/lib/genres.js
@@ -1,7 +1,10 @@
 // tags :: Object
-import tags from '../../assets/newTags.json';
+import tags from '../../assets/tags.json';
 // genresObject :: Object
 import genresObject from '../../assets/genres.json';
+// primaryTagsColors :: Object
+export primaryTagsColors from '../../assets/primaryTagsColors.json';
+
 
 // primaryTags :: [String]
 export const primaryTags = Object.keys(tags);
@@ -9,6 +12,8 @@ export const primaryTags = Object.keys(tags);
 export const getSecondaryTagsOf = primaryTags => primaryTags.reduce((secondaryTags, ptag) => secondaryTags.concat(tags[ptag]), []);
 // getPrimaryTagsFrom :: ([String], [String]) -> [String]
 export const getPrimaryTagsFrom = (tags, prmrTags = primaryTags) => tags.filter(tag => primaryTags.includes(tag));
+// inTags :: (String, [String]) -> Bool
+export const inTags = (tag, tags) => tags.includes(tag);
 
 // genresNames :: [String]
 export const genresNames = Object.keys(genresObject);
