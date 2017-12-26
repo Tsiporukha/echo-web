2ae95e3e8235ea4157f28be9ec2c6d71c2cd9051
surov <brainsurov@gmail.com>
2017-12-26 14:11:52 +0200

    Add PlaylistHolderSong container

---
+++ src/scripts/containers/PlaylistHolderSong.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+import Song from '../components/Song';
+
+import {addClonedSongToTopAndPlay, removePlaylistSong} from '../actions/QueueActions';
+import {setCurrentSong, pause, play} from '../actions/PlayerActions';
+
+
+const mapStateToProps = (state, ownProps) => ({
+  song: state.songs[ownProps.id],
+  isCurrentSong: state.songs[ownProps.id].uid === state.player.currentSong.uid,
+  isPlaying: state.player.playing && state.songs[ownProps.id].uid === state.player.currentSong.uid,
+  // rename to inPlaylist
+  inQueue: true,
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  remove: () => dispatch(removePlaylistSong(ownProps.playlist, ownProps.id)),
+  play: () => dispatch(play()),
+  setCurrentSong: song => () => dispatch(addClonedSongToTopAndPlay(song)),
+  toggleLike: (song, token) => dispatch(toggleSongLike(song, token)),
+
+  pause: () => dispatch(pause()),
+});
+
+
+const PlaylistHolderSong = connect(mapStateToProps, mapDispatchToProps)(Song);
+
+PlaylistHolderSong.propTypes = {
+  playlist: PropTypes.object.isRequired,
+  id: PropTypes.number.isRequired
+};
+
+export default PlaylistHolderSong;
