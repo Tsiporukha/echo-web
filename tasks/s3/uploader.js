9cb983ba08037ea3a408ebfb95bc84134e70c95f
surov <brainsurov@gmail.com>
2018-01-18 16:50:22 +0200

    Add assets/images uploading to s3

---
+++ tasks/s3/uploader.js
@@ -0,0 +1,17 @@
+import s3 from 's3';
+import credentials from './credentials.json';
+
+
+const client = s3.createClient({
+  s3Options: credentials,
+});
+
+
+const imageUploader = client.uploadDir({
+  localDir: './src/assets/images',
+
+  s3Params: {
+    Bucket: credentials.bucket,
+    Prefix: 'assets/images/',
+  },
+});
