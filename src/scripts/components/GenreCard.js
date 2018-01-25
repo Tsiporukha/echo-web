bb72a65d3d427532d1c7e4981d9461f6235d5d00
surov <brainsurov@gmail.com>
2018-01-25 14:49:56 +0200

    Restore old view for GenreCard

--- src/scripts/components/GenreCard.js
+++ src/scripts/components/GenreCard.js
@@ -3,20 +3,23 @@ import {Link} from 'react-router-dom';
 
 import ShareIconMenu from './ShareIconMenu';
 
-import styles from '../../assets/styles/genreCard.css';
+import styles from '../../assets/styles/card.css';
 
 
 const GenreCard = ({genre}) => (
-  <Link to={`/genres/${genre.title}`}>
-    <div className={styles.root} style={{backgroundImage: `url(${genre.background_url})`}}>
-      <div className={styles.description}>
-        {genre.description}
+  <div className={styles.root}>
+    <img className={styles.artwork} src={genre.artwork_url} alt='genre artwork' />
+    <div className={styles.data}>
+      <div className={styles.titleBlock}>
+        <div className={styles.title}>
+          <Link to={`/genres/${genre.title}`}> {genre.title} </Link>
+        </div>
+        <ShareIconMenu path={`/genres/${genre.title}`} picture={genre.artwork_url}
+          title={genre.title} description={genre.description} />
       </div>
-      <div className={styles.title}>
-        {genre.title}
-      </div>
+      <div className={styles.description}> {genre.description} </div>
     </div>
-  </Link>
+  </div>
 );
 
 export default GenreCard;
