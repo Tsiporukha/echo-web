e2aa3f01aec84b6514d3675989e3a92a3813bd95
surov <brainsurov@gmail.com>
2018-02-12 15:12:02 +0200

    Put AppBody to App component

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -16,34 +16,20 @@ import {removeSeparator} from '../lib/route';
 import styles from '../../assets/styles/app.css';
 
 
-class AppBody extends Component {
-
-  componentWillMount = () => removeSeparator(this.props.location.hash, this.props.history, '#!');
-
-  render(){
-    return(
-      <div>
-        <FacebookSDK />
-
-        <Navbar />
-        <div>
-          <Route exact path='/' component={GuestLanding} />
-          <Route exact path='/feed' component={FeedSources} />
-          <Route exact path='/feed/:id' component={Stream} />
-          <Route exact path='/profile/:id' component={Profile} />
-          <Route exact path='/genres/:title' component={Genre} />
-          <Route exact path='/genres/:genre/rooms/:id' component={Room} />
-        </div>
-        <Player />
-      </div>
-    )
-  }
-}
-
-
 const App = () => (
   <BrowserRouter>
-    <Route component={AppBody} />
+    <FacebookSDK />
+
+    <Navbar />
+    <div>
+      <Route exact path='/' component={GuestLanding} />
+      <Route exact path='/feed' component={FeedSources} />
+      <Route exact path='/feed/:id' component={Stream} />
+      <Route exact path='/profile/:id' component={Profile} />
+      <Route exact path='/genres/:title' component={Genre} />
+      <Route exact path='/genres/:genre/rooms/:id' component={Room} />
+    </div>
+    <Player />
   </BrowserRouter>
 );
 
