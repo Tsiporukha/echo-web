b78e7ec56cbf591c7d5226f74dc163e45dede9d1
surov <brainsurov@gmail.com>
2018-03-28 16:18:58 +0300

    Update Notifications with styles

--- src/scripts/components/Notification.js
+++ src/scripts/components/Notification.js
@@ -11,32 +11,30 @@ const getUserName = user => user.username || user.name || '';
 const updateNotificationDescription = (description, username, streamId) =>
   description.replace(`your ${username} friend`, username).slice(username.length).replace(streamId || '', '');
 
-const getNotificationUrl = (key, userId, streamId) =>
-  ['allow_followers', 'allow_on_air'].includes(key) ? `/profile/${userId}` : `/feed/${streamId}`;
+const getNotificationUrl = ({key, user, stream_id}) =>
+  ['allow_followers', 'allow_on_air'].includes(key) ? `/profile/${user.id}` : `/feed/${stream_id}`;
 
 
 const Notification = ({notification}) => (
-  <Link to={getNotificationUrl(notification.key, notification.user.id, notification.stream_id)}>
-    <div className={styles.notification}>
-      <Link to={`/profile/${notification.user.id}`}>
-        <img className={styles.avatar} src={notification.user.avatar_url} alt='avatar' />
-      </Link>
-      <div className={styles.info}>
-        <div>
-          <Link to={`/profile/${notification.user.id}`}>
-            <span className={styles.username}>{notification.user.name}</span>
-          </Link>
-          <Link to={getNotificationUrl(notification.key, notification.user.id, notification.stream_id)}>
-            <span className={styles.message}>
-              {updateNotificationDescription(notification.description, getUserName(notification.user), notification.stream_id)}
-            </span>
-          </Link>
-        </div>
-        {!!notification.comment_text && false && <span>{notification.comment_text}</span>}
-        <div className={styles.time}>{fromNow(new Date(notification.created_at))}</div>
+  <Link to={getNotificationUrl(notification)} className={styles.notification}>
+    <Link to={`/profile/${notification.user.id}`}>
+      <img className={styles.avatar} src={notification.user.avatar_url} alt='avatar' />
+    </Link>
+    <div className={styles.info}>
+      <div>
+        <Link to={`/profile/${notification.user.id}`}>
+          <span className={styles.username}>{notification.user.name}</span>
+        </Link>
+        <Link to={getNotificationUrl(notification.key, notification.user.id, notification.stream_id)}>
+          <span className={styles.message}>
+            {updateNotificationDescription(notification.description, getUserName(notification.user), notification.stream_id)}
+          </span>
+        </Link>
       </div>
-      {!notification.readed_at && <i className={styles.unreaded} />}
+      {!!notification.comment_text && false && <span>{notification.comment_text}</span>}
+      <div className={styles.time}>{fromNow(new Date(notification.created_at))}</div>
     </div>
+    {!notification.readed_at && <i className={styles.unreaded} />}
   </Link>
 );
 
