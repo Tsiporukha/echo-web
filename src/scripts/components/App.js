7550411c5d3291d3834dbcc0c33b6140e5401fa5
surov <brainsurov@gmail.com>
2018-04-02 14:18:52 +0300

    Hide Genre, Room and ProfileEditing pages

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -4,11 +4,11 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import FacebookSDK from './FacebookSDK';
 import Navbar from './Navbar';
 import FeedSources from './FeedSources';
-import Genre from '../containers/Genre';
-import Room from '../containers/Room';
+// import Genre from '../containers/Genre';
+// import Room from '../containers/Room';
 import Stream from '../containers/Stream';
 import Profile from '../containers/Profile';
-import ProfileEditing from '../containers/ProfileEditing';
+// import ProfileEditing from '../containers/ProfileEditing';
 import Player from '../containers/Player';
 
 import styles from '../../assets/styles/app.css';
@@ -24,9 +24,9 @@ const App = () => (
         <Route exact path='/' component={FeedSources} />
         <Route exact path='/feed/:id' component={Stream} />
         <Route exact path='/profile/:id' component={Profile} />
-        <Route exact path='/settings' component={ProfileEditing} />
+        {/* <Route exact path='/settings' component={ProfileEditing} />
         <Route exact path='/genres/:title' component={Genre} />
-        <Route exact path='/genres/:genre/rooms/:id' component={Room} />
+        <Route exact path='/genres/:genre/rooms/:id' component={Room} /> */}
       </div>
       <Player />
     </div>
--- src/scripts/components/FeedSources.js
+++ src/scripts/components/FeedSources.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Tab, Tabs} from 'react-toolbox/lib/tabs';
 
-import Genres from './Genres';
+// import Genres from './Genres';
 import Feed from '../containers/Feed';
 import PopularSongs from '../containers/PopularSongs';
 import RecentlyLiked from '../containers/RecentlyLiked';
@@ -23,8 +23,8 @@ export default class FeedSources extends Component {
       <div className={styles.root}>
         <div className={`${styles.left} ${tabsTheme.feed}`}>
           <Tabs theme={tabsTheme} index={this.state.index} onChange={this.handleTabChange}>
-            <Tab label={<i className={styles.genresIcon}>forum</i>}> <Genres /> </Tab>,
-            <Tab label={<i className={styles.feedIcon}>language</i>}> <Feed /> </Tab>,
+            {/* <Tab label={<i className={styles.genresIcon}>forum</i>}> <Genres /> </Tab>, */}
+            <Tab label={<i className={styles.feedIcon}>language</i>}> <Feed /> </Tab>
             <Tab label={<i className={styles.whatshotIcon}>whatshot</i>}> <PopularSongs /> </Tab>
           </Tabs>
         </div>
